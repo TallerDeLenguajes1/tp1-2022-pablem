51ace59e4b518b0585903667fe31c396d5b0b88d
Abregu Braian Marcelo <>
2022-08-26 20:44:09 -0300

    primer commit

--- Program.cs
+++ Program.cs
@@ -2,26 +2,48 @@
     return x * x;
 }
 
-try
+static float division(float a, float b) {
+    return a/b;
+}
+
+static float kilomLitro(float k, float l) {
+
+}
+
+/* try
 {
+    System.Console.WriteLine("1. Ingrese un númnero");
     int num = Convert.ToInt32(Console.ReadLine());
     
     num = cuadrado(num);
     
     Console.WriteLine(num);
 }
-catch (FormatException e)
+catch (FormatException)
 {
-    Console.WriteLine("Error: "+ e.Message );
+    Console.WriteLine("Error: Fordmato incorrecto");
     // throw;
 }
-catch (OverflowException e)
+catch (OverflowException)
 {
-    Console.WriteLine("Error: "+ e.Message );
+    Console.WriteLine("Error: Desbordamiento");
     // throw;
 }
 catch (System.Exception e)
 {
     Console.WriteLine("Error: "+ e.Message );
     // throw;
+} */
+try
+{
+	    System.Console.WriteLine("1. Ingrese el primer número: ");
+	    float a = float.Parse(Console.ReadLine());
+	    System.Console.WriteLine("2. Ingrese el segundo número: ");
+	    float b = float.Parse(Console.ReadLine());
+	    System.Console.WriteLine($"El resultado es: {division(a,b)}");
+}
+catch (System.Exception)
+{
+	
+	throw;
 }
