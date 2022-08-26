05464bf0bc9858d5cdb0e14a5d2d2b46f3245b5c
Abregu Braian Marcelo <>
2022-08-26 19:51:10 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿static int cuadrado(int x){
+    return x * x;
+}
+
+try
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    num = cuadrado(num);
+    
+    Console.WriteLine(num);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Error: "+ e.Message );
+    // throw;
+}
+catch (OverflowException e)
+{
+    Console.WriteLine("Error: "+ e.Message );
+    // throw;
+}
+catch (System.Exception e)
+{
+    Console.WriteLine("Error: "+ e.Message );
+    // throw;
+}
